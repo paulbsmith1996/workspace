bd1ba27e815a23d8f2afe785102f7b7fbad1bb55
Paul Baird-Smith <paul.bairdsmith@gmail.com>
2017-01-04 16:03:39 +0100

    Reverted test change

--- Euler/src/problems/P1.java
+++ Euler/src/problems/P1.java
@@ -3,7 +3,6 @@ package problems;
 public class P1 {
 
 	public static void main(String[] args) {
-		System.out.println("Test change for github");
 		System.out.println(1000/3 + 1000/5 - 1000/15);
 	}
 }
