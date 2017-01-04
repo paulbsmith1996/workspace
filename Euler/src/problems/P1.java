289b2b321f486dde5e1fd874e9d0cb49c4d6968c
Paul Baird-Smith <paul.bairdsmith@gmail.com>
2017-01-04 16:01:12 +0100

    This is a change to test our github setup.

--- Euler/src/problems/P1.java
+++ Euler/src/problems/P1.java
@@ -3,6 +3,7 @@ package problems;
 public class P1 {
 
 	public static void main(String[] args) {
+		System.out.println("Test change for github");
 		System.out.println(1000/3 + 1000/5 - 1000/15);
 	}
 }
